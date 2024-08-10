e617ab80f0da482b345e70d3c08cd512bb9be96f
VenkataManoj2424 <kvvmanoj2424@gmail.com>
2024-08-10 19:53:53 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,29 @@
+#list using slicing
+L=[10,20,'hi',44.4,'hello']
+print(L[0:3])
+
+#
+P=[1,2,3,4,5]
+'''P[0]=5
+P[4]=1
+print(P)'''
+
+'''temp=P[0]
+P[4]=P[0]
+P[4]=temp
+print(P)'''
+
+#addition
+'''m=[1,2,3,4,5]
+s=0
+for i in m:
+    s=s+i
+print(s)'''
+
+'''t=[1,1,2,1,3,4,5,2,5,4,8,2,3,5,4]
+s=[]
+for i in t:
+    if i not in s:
+        s.append(i)
+print(s)'''
+
